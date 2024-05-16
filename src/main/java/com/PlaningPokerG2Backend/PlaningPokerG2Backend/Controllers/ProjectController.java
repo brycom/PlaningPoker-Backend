459bf40a593e87d3190e33a002bf4a821bae01e4
Code-Cadde <alexander.cadier@hotmail.com>
2024-05-16 10:32:01 +0200

    new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/AuthorizationController.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/IssuesController.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/ProjectController.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/RoleController.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/StatisticsController.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/UserController.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/VoteController.java 	renamed:    src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/testController.java -> src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/testController.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Services/AuthorizationService.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Services/IssuesService.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Services/ProjectService.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Services/RoleService.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Services/StatisticsService.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Services/UserService.java 	new file:   src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Services/VoteService.java 	modified:   src/main/resources/application.properties

---
+++ src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/ProjectController.java
@@ -0,0 +1,5 @@
+package com.PlaningPokerG2Backend.PlaningPokerG2Backend.Controllers;
+
+public class ProjectController {
+    
+}
--- src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/testController.java
+++ src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/testController.java
@@ -1,4 +1,4 @@
-package com.PlaningPokerG2Backend.PlaningPokerG2Backend;
+package com.PlaningPokerG2Backend.PlaningPokerG2Backend.Controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;

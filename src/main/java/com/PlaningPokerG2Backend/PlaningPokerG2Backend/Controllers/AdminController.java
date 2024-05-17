d048eba8656c396160f4055d2525b6d8a394abe2
Kurvel <143402591+Kurvel@users.noreply.github.com>
2024-05-17 09:15:15 +0200

    WIP admin endpoints

--- src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/AdminController.java
+++ src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/AdminController.java
@@ -2,11 +2,13 @@ package com.PlaningPokerG2Backend.PlaningPokerG2Backend.Controllers;
 
 import java.util.List;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Models.Issues;
@@ -15,7 +17,9 @@ import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Models.Role;
 import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Models.User;
 import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Services.AdminService;
 
+
 @RestController
+@RequestMapping("/admin")
 public class AdminController {
     
     private AdminService adminService;
@@ -35,13 +39,13 @@ public class AdminController {
     }
 
     
-    // issues
+    
     @GetMapping("/issues")
     public List<Issues> getIssues() {
         return adminService.getIssues();
     }
 
-    // edit roles
+    
     @PatchMapping("/role/{id}") 
         public Role editRole(@PathVariable int id, @RequestBody Role role) {
             return adminService.editRole(id, role);

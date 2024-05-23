ee232016570ecb30b4ff1bee7e55c6c53c5f483d
Kurvel <143402591+Kurvel@users.noreply.github.com>
2024-05-23 11:11:13 +0200

    vote klar

--- src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/VoteController.java
+++ src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/VoteController.java
@@ -1,9 +1,8 @@
 package com.PlaningPokerG2Backend.PlaningPokerG2Backend.Controllers;
 
-
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Models.Vote;
-import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Repositories.CustomProjectRepository;
 import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Services.VoteService;
 
 @CrossOrigin(origins = "*")
@@ -22,38 +20,36 @@ import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Services.VoteService;
 @RequestMapping("/vote")
 public class VoteController {
 
-    @Autowired
-    private CustomProjectRepository customProjectRepository;
+    
 
     private VoteService voteService;
-    
 
-    
-
-    public VoteController(CustomProjectRepository customProjectRepository, VoteService voteService) {
-        this.customProjectRepository = customProjectRepository;
+    public VoteController(VoteService voteService) {  
         this.voteService = voteService;
     }
 
     @GetMapping("/votes/{projectId}/{issueId}")
     public List<Vote> getVotes(@PathVariable String projectId, @PathVariable String issueId) {
 
-        List<Vote> votes = customProjectRepository.findVotesByIssueId(projectId, issueId);
+        List<Vote> votes = voteService.findVotesByIssueId(projectId, issueId);
         return votes;
     }
 
-    // add vote to issue with userId
     @PostMapping("/uservote/{projectId}/{issueId}")
     public Vote addUserVote(@RequestBody Vote vote, @PathVariable String projectId, @PathVariable String issueId) {
-        customProjectRepository.addUserVote(vote, projectId, issueId);
+        voteService.addUserVote(vote, projectId, issueId);
         return vote;
     }
 
-    // reset vote
-    @DeleteMapping("/deleteVotes{projectId}/{issueId}")
+    @DeleteMapping("/deletevotes/{projectId}/{issueId}")
     public String resetVotes(@PathVariable String projectId, @PathVariable String issueId) {
-        
-        return "delete";
+
+        return voteService.resetVotes(projectId, issueId);
+    }
+
+    @GetMapping("/uservote/{projectId}/{issueId}/{userId}")
+    public Vote getUserVote(@PathVariable String projectId, @PathVariable String issueId, @PathVariable String userId) {
+        return voteService.getUserVote(projectId, issueId, userId);
     }
 
 }
--- src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Services/VoteService.java
+++ src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Services/VoteService.java
@@ -1,29 +1,120 @@
 package com.PlaningPokerG2Backend.PlaningPokerG2Backend.Services;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
+import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Models.Issue;
 import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Models.Project;
 import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Models.Vote;
 
 @Service
 public class VoteService {
-    
+
     private final MongoOperations mongoOperations;
 
     public VoteService(MongoOperations mongoOperations) {
         this.mongoOperations = mongoOperations;
     }
 
-    // public Vote addUserVote(Vote vote, String projectId, String issueId ) {
-    //     System.out.println("proj: " + projectId);
-    //     System.out.println("issue: " + issueId);
+    public List<Vote> findVotesByIssueId(String projectId, String issueId) {
+        System.out.println("Project ID: " + projectId);
+        System.out.println("Issue ID: " + issueId);
 
-    //     Query query = new Query(Criteria.where("id").is(projectId));
-    //     Project project = mongoOperations.findOne(query, Project.class);
-    //     System.out.println("project: " + project);
+        Project project = mongoOperations.findById(projectId, Project.class);
 
-    // }
+        if (project == null) {
+            System.out.println("Project not found");
+            return null;
+        }
+
+        List<Issue> allIssues = project.getIssues();
+        System.out.println("allIssues" + allIssues);
+
+        for (Issue issue : allIssues) {
+            if (issue.getIssueId().equals(issueId)) {
+
+                return issue.getVotes();
+            }
+        }
+
+        System.out.println("Issue not found");
+        return null;
+    }
+
+    public Vote addUserVote(Vote vote, String projectId, String issueId) {
+
+        Project project = mongoOperations.findById(projectId, Project.class);
+
+        if (project == null) {
+            System.out.println("Project not found");
+            return null;
+        }
+
+        List<Issue> allIssues = project.getIssues();
+        System.out.println("allIssues: " + allIssues);
+
+        for (Issue issue : allIssues) {
+            if (issue.getIssueId().equals(issueId)) {
+                issue.getVotes().add(vote);
+                mongoOperations.save(project);
+                return vote;
+            }
+        }
+
+        System.out.println("Issue not found");
+        return null;
+    }
+
+    public String resetVotes(String projectId, String issueId) {
+
+        Project project = mongoOperations.findById(projectId, Project.class);
+
+        if (project == null) {
+            System.out.println("Project not found");
+            return "Project not found";
+        }
+
+        List<Issue> allIssues = project.getIssues();
+        System.out.println("allIssues: " + allIssues);
+
+        for (Issue issue : allIssues) {
+            if (issue.getIssueId().equals(issueId)) {
+
+                issue.getVotes().clear();
+
+                mongoOperations.save(project);
+
+                return "Votes reset successfully";
+            }
+        }
+        return "Issue not found";
+    }
+
+    public Vote getUserVote(String projectId, String issueId, String userId) {
+        Project project = mongoOperations.findById(projectId, Project.class);
+
+        if (project == null) {
+            System.out.println("Project not found");
+            return null;
+        }
+        List<Issue> allIssues = project.getIssues();
+        for (Issue issue : allIssues) {
+            if (issue.getIssueId().equals(issueId)) {
+
+                List<Vote> votes = issue.getVotes();
+
+                for (Vote vote : votes) {
+                    if (vote.getUserId().equals(userId)) {
+                        return vote;
+                    }
+                }
+                System.out.println("Vote not found for userId: " + userId);
+                return null;
+            }
+        }
+        System.out.println("Issue not found");
+        return null;
+    }
 }

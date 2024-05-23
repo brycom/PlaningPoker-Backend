6dda8f32e50d5d309efbdb84e4afbbdcea044b38
Mathias Brynolf <mathiasbrynolf20@gmail.com>
2024-05-23 14:31:35 +0200

    .

--- src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/ProjectController.java
+++ src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/ProjectController.java
@@ -7,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +14,11 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Models.Project;
 import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Models.User;
-import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Security.SecurityConfig;
-import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Security.Utilitys.RSAKeyProperties;
 import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Services.ProjectService;
 import com.PlaningPokerG2Backend.PlaningPokerG2Backend.Services.TokenService;
 

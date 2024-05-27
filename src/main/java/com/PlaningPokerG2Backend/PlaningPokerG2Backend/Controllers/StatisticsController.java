08484d3e8b03adc1e32cbfa3b2499d64c52bcc1b
tegmen88 <cite7173@gmail.com>
2024-05-27 12:07:46 +0200

    CrossOrigin i statsticcontroller

--- src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/StatisticsController.java
+++ src/main/java/com/PlaningPokerG2Backend/PlaningPokerG2Backend/Controllers/StatisticsController.java
@@ -1,6 +1,7 @@
 package com.PlaningPokerG2Backend.PlaningPokerG2Backend.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/statistics")
+@CrossOrigin("*")
 public class StatisticsController {
 
     @Autowired
